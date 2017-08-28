c8354bb054ec9a4f5465574e4e15db864af66f6b
zeroxys <miguelzavalac@gmail.com>
2017-08-28 18:11:04 -0500

    nacbar

--- dist/bundle.js
+++ dist/bundle.js
@@ -110,32 +110,11 @@ __webpack_require__(21);
 
 __webpack_require__(22);
 
-// $('.btn-menu').click(function(event) {
-//     $('.menu-des').css({'height' : alto});
-//       if(!$('.menu-des').hasClass('abierto')){
-//         $('.menu-des').slideToggle(400).addClass('abierto');
-//         $('.btn-menu').css({
-//           'background-image': 'url(img/close2.png)'
-//         });
-//        // $('.menu-des').animate({'right': 0}, 500).addClass('abierto');
-//       }else{
-//         $('.menu-des').slideToggle(400).removeClass('abierto');
-//         $('.btn-menu').css({
-//           'background-image': 'url(img/menu.png)'
-//         });
-//          // $('.menu-des').slideToggle(1200);
-//       }
-//   });
-
 //contacto
-$('nav').click(function (event) {
-  if ($('.menu-des').hasClass('abierto')) {
-    //$('.menu-des').animate({'right': '-50%'}, 500).removeClass('abierto');
-    $('.menu-des').slideToggle(400).removeClass('abierto');
-    $('.btn-menu').css({
-      'background-image': 'url(img/menu.png)'
-    });
-  }
+$(document).ready(function () {
+  $('.responsive-button').on('click', function () {
+    $('.navList').toggleClass('show');
+  });
 });
 
 // auditoria
